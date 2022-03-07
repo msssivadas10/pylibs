1d248031b3f1340ca05d95270bdedd6961a8c92b
M. S. Suryan Sivadas <ms3.msssivadas@gmail.com>
2022-03-07 10:19:24 +0530

    Updated tables.py

--- pylibs_expt/tables.py
+++ pylibs_expt/tables.py
@@ -1,6 +1,6 @@
 from typing import Any, Iterable, List, Type
 from collections import namedtuple
-import numpy as np, re
+import numpy as np, re, json
 
 class TableError(Exception):
     """ Base class exceptions used by table objects. """
@@ -39,6 +39,7 @@ class Table:
 
     """
     __slots__ = '_nr', '_nc', '_subset_getter', 
+    __name__  = ''
 
     def __init__(self, *args, **kwargs) -> None:
         self._nr: int = ...
@@ -140,7 +141,6 @@ class Table:
         """ Print the table. """
         ...
 
-
 class TableSubsetGetter:
     """ 
     Instances of this class are used by table objects to get subsets of a table. 
@@ -178,6 +178,29 @@ class TableSubsetGetter:
 
         cls = table(self._table.__name__, __cols)
         return cls(**{__col: self._table[__col][__rows] for __col in __cols})
+
+class JSONEncoder(json.JSONEncoder):
+    """
+    Custom JSON encoder for table objects.
+    """
+
+    def default(self, o: Any) -> Any:
+        if not isinstance(o, Table):
+            return super().default(o)
+        
+        # json format of the table is {"key": ["type", *data], ...}
+        _dict = {}
+        for key in o.colnames:
+            value = o[key]
+
+            # get column type name:
+            tp_name  = value.dtype.name
+            tp, size =re.search(r'([a-z]+)(\d*)', tp_name).groups()
+            if tp not in ['bool', 'int', 'float', 'complex', 'str']:
+                raise TypeError(f"type '{tp_name}' is not supported")
+            
+            _dict[key] = [tp_name, *list(value)]
+        return _dict
 
 def table(name: str, cols: Iterable[str]) -> Type[Table]:
     """ 
@@ -343,10 +366,121 @@ def table(name: str, cols: Iterable[str]) -> Type[Table]:
                     }
                 )
 
+def extent(old: Type[Table], cols: Iterable[str], name: str = ..., ) -> Type[Table]:
+    """
+    Extent an existing table type with new columns. This can be used to get the 
+    functionality equivalent to adding new columns. Since a table type does not 
+    support adding new columns to it, a new table type is created with columns 
+    from the old table and the extra columns given. 
+
+    Parameters
+    ----------
+    old: type, subclass of :class:`Table`
+        Table type to be extented.
+    cols: list or str
+        Names of the extra columns. Must be different from the columns in the old.
+    name: str, optional
+        Type name for the new table type. If not given, use the same name as the 
+        previous.
+
+    Returns
+    -------
+    table_t: type, subclass of :class:`Table`
+        A table type extented from the old.
+
+    Examples
+    --------
+    >>> old_table = table('old_table', ['x', 'y']) # old table
+    >>> new_table = extent(old_table, ['z', 'w'])  # new table
+    >>> 
+    >>> # create a table of 'new_table' format:
+    >>> t = new_table([0., 1.], [0., 1.], [0., 1.], [0., 1.])
+    >>> t
+    <table 'old_table': cols=('x', 'y', 'z', 'w'), shape=(2, 4)>
+
+    """
+    if not issubclass(old, Table):
+        raise TypeError("old must be a subclass of 'Table'")
+    if not cols:
+        raise ValueError("cols cannot be empty")
+
+    name = old.__name__ if name is ... else name
+    cols = [_name for _name in [*old.__slots__, *cols] if _name not in Table.__slots__]
+    return table(name, cols)
+
+def create(table_t: Type[Table], *args, **kwargs) -> Table:
+    """
+    Create a table of given type and fill values to it. These values are taken 
+    from the other tables (given as positional arguments) and arrays (given as 
+    keyword arguments, where the key is the column name). This can be used to 
+    initialise a table extented from an existing one. For example,
+
+    >>> # create atable with two columns x and y
+    >>> my_table = table('my_table', ['x', 'y'])
+    >>> t1       = my_table([1.0, 2.0, 3.0], [0.9, 2.1, 3.2])
+    >>> # extent the table by adding another column z
+    >>> my_table2 = extent(my_table, ['z'])
+    >>> # fill the table with data from the old table and an z array
+    >>> t2 = create(my_table2, t1, z = [0.3, 0.1, 0.2])
+    >>> t2
+    <table 'my_table': cols=('x', 'y', 'z'), shape=(3, 3)>
+
+    """
+    if not issubclass(table_t, Table):
+        raise TypeError("table_t must be a subclass of 'Table'")
+    
+    # get column names:
+    cols  = {_name: None for _name in table_t.__slots__ if _name not in Table.__slots__}
+    nrows = 0
+
+    # get columns from the given tables:
+    for _tb in args:
+        if not isinstance(_tb, Table):
+            raise TypeError("argument must be a 'Table' object")
+        for key in _tb.colnames:
+            # check if this column is present in the output table
+            if key not in cols.keys():
+                continue
+
+            # check this column is already set, meaning a previous table 
+            # has a column with this name, which is not allowed 
+            if cols[key] is not None:
+                raise TableError(f"multiple tables has column '{key}'")
+            cols[key] = _tb[key]
+
+            if not nrows:
+                nrows = _tb.nr
+            elif nrows != _tb.nr:
+                raise TableError("all tables must have the same number of rows")
+
+    # get other columns:
+    for key, value in kwargs.items():
+        if key not in cols.keys():
+            raise TableError(f"no column called '{key}' in the table")
+        if not nrows:
+            nrows = len(value)
+        elif nrows != len(value):
+            raise TableError("length of the array must be equal to the number of rows")
+        
+        cols[key] = value
+
+    # check if all columns are set:
+    for key, value in cols.items():
+        if value is None:
+            raise TableError(f"no values are given for column '{key}'")
+
+    return table_t(**cols) # make the table
+                
+
 
 
 if __name__ == "__main__":
     my_table = table('my_table', ['x', 'y'])
     t1       = my_table([1.0, 2.0, 3.0], [0.9, 2.1, 3.2])
 
-    print(t1.subset[:2, ['x']])
+    my_table2 = extent(my_table, ['z'])
+
+    t2 = create(my_table2, t1, z = [0.3, 0.1, 0.2])
+    print(json.dumps(t2, cls = JSONEncoder, indent = 4))
+
+    
